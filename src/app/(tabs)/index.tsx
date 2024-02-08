ea9cb23ad048fdbb8ef39b4aeed54dbfed780f00
n8urm8 <81867790+n8urm8@users.noreply.github.com>
2024-02-08 11:23:10 -0500

    list

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,38 +1,24 @@
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, FlatList, Image } from "react-native";
 
-import EditScreenInfo from "@/src/components/EditScreenInfo";
 import Colors from "@/src/constants/Colors";
 import products from "@/assets/data/products";
-
-const product = products[1];
+import { ProductListItem } from "@/src/components/ProductListItem";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}</Text>
-    </View>
+    <FlatList
+      style={styles.productList}
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 15,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
+  productList: {
+    gap: 5,
   },
 });
